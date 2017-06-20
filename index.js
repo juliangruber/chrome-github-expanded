3c747770d0e7e932da6e4bb82617c8e4add03a36
Julian Gruber <julian@juliangruber.com>
2017-06-20 21:10:20 +0200

    load only diffs matching regexp

--- index.js
+++ index.js
@@ -3,7 +3,17 @@
 document.addEventListener('DOMContentLoaded', () => {
   const loop = () => {
     const buttons = Array.from(document.querySelectorAll('.load-diff-button'))
-    for (let button of buttons) button.click()
+    for (let button of buttons) {
+      let header
+      let container = button
+      while (container = container.parentNode) {
+        header = container.querySelector('.file-header')
+        if (header) break
+      }
+      if (/^[^\/]+\/node_modules\//.test(header.dataset.path)) {
+        button.click()
+      }
+    }
     if (buttons.length) requestAnimationFrame(loop)
   }
   loop()
